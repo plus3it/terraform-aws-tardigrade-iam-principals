31701d412cc990980b567ac1ae9cf0af9a431287
Loren Gordon <lorengordon@users.noreply.github.com>
2020-09-21 13:40:21 -0700

    Creates singular role instead of multiple

---
+++ modules/role/README.md
@@ -0,0 +1,44 @@
+# terraform-aws-tardigrade-iam-principals
+
+Terraform module to create IAM roles
+
+
+<!-- BEGIN TFDOCS -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 0.13 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| aws | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| assume\_role\_policy | Schema map of attributes for the assume role policy, see `policy_document` for attribute descriptions | <pre>object({<br>    template       = string<br>    template_paths = list(string)<br>    template_vars  = map(string)<br>  })</pre> | n/a | yes |
+| name | Name of the IAM role | `string` | n/a | yes |
+| description | Description of the IAM role | `string` | `null` | no |
+| force\_detach\_policies | Boolean to control whether to force detach any policies the role has before destroying it | `bool` | `null` | no |
+| inline\_policies | Schema list of IAM User inline policies, see `policy_document` for attribute descriptions | <pre>list(object({<br>    name           = string<br>    template       = string<br>    template_paths = list(string)<br>    template_vars  = map(string)<br>  }))</pre> | `[]` | no |
+| instance\_profile | Boolean to control whether to create an instance profile for the role | `bool` | `false` | no |
+| managed\_policies | Schema list of IAM managed policies to attach to the user, including the policy `name` and `arn` | <pre>list(object({<br>    name = string<br>    arn  = string<br>  }))</pre> | `[]` | no |
+| max\_session\_duration | Maximum session duration (in seconds) to set for the role. The default is one hour. This setting can have a value from 1 hour to 12 hours | `number` | `null` | no |
+| path | Path for the role | `string` | `null` | no |
+| permissions\_boundary | ARN of the managed policy to set as the permissions boundary for the user | `string` | `null` | no |
+| tags | Map of tags to apply to all resources that support tags | `map(string)` | `{}` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| inline\_policies | IAM inline policy attachment resources |
+| instance\_profile | IAM instance profile resource |
+| managed\_policies | IAM managed policy attachment resources |
+| roles | IAM role resource |
+
+<!-- END TFDOCS -->
--- tests/create_roles/main.tf
+++ tests/create_roles/main.tf
@@ -2,23 +2,26 @@ provider aws {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
+module create_roles {
+  source   = "../..//modules/role"
+  for_each = { for role in local.roles : role.name => merge(local.role_base, role) }
 
-data "aws_partition" "current" {}
+  name               = each.key
+  assume_role_policy = each.value.assume_role_policy
 
-data "aws_region" "current" {}
-
-data "terraform_remote_state" "prereq" {
-  backend = "local"
-  config = {
-    path = "prereq/terraform.tfstate"
-  }
+  description           = each.value.description
+  force_detach_policies = each.value.force_detach_policies
+  inline_policies       = each.value.inline_policies
+  instance_profile      = each.value.instance_profile
+  managed_policies      = each.value.managed_policies
+  max_session_duration  = each.value.max_session_duration
+  path                  = each.value.path
+  permissions_boundary  = each.value.permissions_boundary
+  tags                  = each.value.tags
 }
 
 locals {
-  test_id = length(data.terraform_remote_state.prereq.outputs) > 0 ? data.terraform_remote_state.prereq.outputs.random_string.result : ""
-
-  policy_arns = length(data.terraform_remote_state.prereq.outputs) > 0 ? [for policy in data.terraform_remote_state.prereq.outputs.policies : policy.arn] : []
+  test_id = data.terraform_remote_state.prereq.outputs.random_string.result
 
   policy_base = {
     path          = null
@@ -36,7 +39,7 @@ locals {
     random_string = random_string.this.result
   }
 
-  inline_policies = [
+  inline_policies = [for policy in [
     {
       name     = "tardigrade-alpha-${local.test_id}"
       template = "policies/template.json"
@@ -45,71 +48,59 @@ locals {
       name     = "tardigrade-beta-${local.test_id}"
       template = "policies/template.json"
     },
-  ]
+  ] : merge(local.policy_base, policy)]
+
+  managed_policies = [for object in data.terraform_remote_state.prereq.outputs.policies : {
+    name = object.policy.name
+    arn  = object.policy.arn
+  }]
 
   role_base = {
+    assume_role_policy    = local.assume_role_policy
     description           = null
     force_detach_policies = null
-    inline_policy_names   = []
+    inline_policies       = []
     instance_profile      = null
+    managed_policies      = []
     max_session_duration  = null
     path                  = null
-    permissions_boundary  = data.terraform_remote_state.prereq.outputs.policies["tardigrade-alpha-create-roles-test"].arn
-    policy_arns           = []
+    permissions_boundary  = data.terraform_remote_state.prereq.outputs.policies["tardigrade-alpha-create-roles-test"].policy.arn
     tags                  = {}
   }
 
-  assume_role_policies = [
-    for role in local.roles : {
-      name           = role.name
-      template       = "trusts/template.json"
-      template_paths = ["${path.module}/../templates/"]
-      template_vars  = local.template_vars_base
-    }
-  ]
-
-  role_inline_policies = [
-    {
-      name            = "tardigrade-role-alpha-${local.test_id}"
-      inline_policies = [for policy in local.inline_policies : merge(local.policy_base, policy)]
-    },
-    {
-      name            = "tardigrade-role-beta-${local.test_id}"
-      inline_policies = [for policy in local.inline_policies : merge(local.policy_base, policy)]
-    },
-    {
-      name            = "tardigrade-role-delta-${local.test_id}"
-      inline_policies = [for policy in local.inline_policies : merge(local.policy_base, policy)]
-    },
-  ]
+  assume_role_policy = {
+    template       = "trusts/template.json"
+    template_paths = ["${path.module}/../templates/"]
+    template_vars  = local.template_vars_base
+  }
 
   roles = [
     {
       name                  = "tardigrade-role-alpha-${local.test_id}"
-      policy_arns           = local.policy_arns
-      inline_policy_names   = local.inline_policies[*].name
+      inline_policies       = local.inline_policies
+      managed_policies      = local.managed_policies
       description           = "Managed by Terraform - Tardigrade test policy"
       force_detach_policies = false
       max_session_duration  = 3600
       path                  = "/tardigrade/alpha/"
-      permissions_boundary  = data.terraform_remote_state.prereq.outputs.policies["tardigrade-beta-create-roles-test"].arn
+      permissions_boundary  = data.terraform_remote_state.prereq.outputs.policies["tardigrade-beta-create-roles-test"].policy.arn
       tags = {
         Env = "tardigrade"
       }
     },
     {
       name                 = "tardigrade-role-beta-${local.test_id}"
-      policy_arns          = local.policy_arns
-      inline_policy_names  = local.inline_policies[*].name
+      inline_policies      = local.inline_policies
+      managed_policies     = local.managed_policies
       permissions_boundary = null
     },
     {
-      name        = "tardigrade-role-chi-${local.test_id}"
-      policy_arns = local.policy_arns
+      name             = "tardigrade-role-chi-${local.test_id}"
+      managed_policies = local.managed_policies
     },
     {
-      name                = "tardigrade-role-delta-${local.test_id}"
-      inline_policy_names = local.inline_policies[*].name
+      name            = "tardigrade-role-delta-${local.test_id}"
+      inline_policies = local.inline_policies
     },
     {
       name = "tardigrade-role-epsilon-${local.test_id}"
@@ -121,30 +112,26 @@ locals {
   ]
 }
 
-resource "random_string" "this" {
+resource random_string this {
   length  = 6
   upper   = false
   special = false
   number  = false
 }
 
-module "create_roles" {
-  source = "../../modules/roles/"
+data aws_caller_identity current {}
 
-  providers = {
-    aws = aws
-  }
+data aws_partition current {}
 
-  assume_role_policies = local.assume_role_policies
-  inline_policies      = local.role_inline_policies
-  policy_arns          = local.policy_arns
-  roles                = [for role in local.roles : merge(local.role_base, role)]
+data aws_region current {}
 
-  tags = {
-    Test = "true"
+data terraform_remote_state prereq {
+  backend = "local"
+  config = {
+    path = "prereq/terraform.tfstate"
   }
 }
 
-output "create_roles" {
+output create_roles {
   value = module.create_roles
 }
