36cc45516fbe27cab476f898dc77999a19b084df
Loren Gordon <lorengordon@users.noreply.github.com>
2022-08-22 09:20:31 -0700

    Uses precondition to ensure that policy document is never empty

--- modules/policy_document/outputs.tf
+++ modules/policy_document/outputs.tf
@@ -1,4 +1,9 @@
 output "policy_document" {
   description = "Rendered and templated policy document"
   value       = data.aws_iam_policy_document.this.json
+
+  precondition {
+    condition     = data.aws_iam_policy_document.this.json != local.empty
+    error_message = "Policy resolved to an empty document, which is never valid. Ensure the provided `template` exists in at least one `template_paths` location. template = ${var.template}. template_paths = ${jsonencode(var.template_paths)}."
+  }
 }
