11151e6e57db7bb67f6d6576e388123563594d32
Loren Gordon <lorengordon@users.noreply.github.com>
2024-09-20 07:43:57 -0700

    Updates iam role module to use exclusive pattern for all policy types

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -5,18 +5,9 @@ resource "aws_iam_role" "this" {
 
   description           = var.description
   force_detach_policies = var.force_detach_policies
-  managed_policy_arns   = concat([for name, policy in data.aws_iam_policy.this : policy.arn], var.managed_policy_arns)
   max_session_duration  = var.max_session_duration
   path                  = var.path
   permissions_boundary  = var.permissions_boundary
-
-  dynamic "inline_policy" {
-    for_each = var.inline_policies
-    content {
-      name   = inline_policy.value.name
-      policy = inline_policy.value.policy
-    }
-  }
 
   tags = merge(
     {
@@ -31,6 +22,33 @@ resource "aws_iam_role" "this" {
       error_message = "The combination of `managed_policy_arns` and `managed_policies` exceeds role limit of 20 total policies."
     }
   }
+}
+
+# attach inline policies to the IAM role
+resource "aws_iam_role_policy" "this" {
+  for_each = { for policy in var.inline_policies : policy.name => policy }
+
+  name   = each.value.name
+  policy = each.value.policy
+  role   = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policies_exclusive" "this" {
+  role_name    = aws_iam_role.this.name
+  policy_names = [for name, policy in aws_iam_role_policy.this : policy.name]
+}
+
+# attach managed policies to the IAM role
+resource "aws_iam_role_policy_attachment" this {
+    for_each = data.aws_iam_policy.this
+
+    role       = aws_iam_role.this.id
+    policy_arn = each.value.arn
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "this" {
+  role_name    = aws_iam_role.this.name
+  policy_arns  = [for name, policy in aws_iam_role_policy_attachment.this : policy.policy_arn]
 }
 
 # attach an instance profile to the IAM role
